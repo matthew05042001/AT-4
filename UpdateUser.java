3fadf710226d7b6d9879f662eeca101eb3a35e29
Matthew Ikee M. Crispino <mcrispino_240000000173@uic.edu.ph>
2025-02-28 17:21:59 +0800

    Add files via upload

---
+++ UpdateUser.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author matth
+ */
+class UpdateUser {
+
+    void filecheck() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void updateUser() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
